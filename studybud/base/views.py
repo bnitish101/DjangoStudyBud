92f30e13e3a771a1366fd9e096f4c083decb362c
Nitish <bnitish101@gmail.com>
2022-02-13 21:33:08 +0530

    CRUD - [Read, Create] data

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,6 +1,7 @@
 # from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
@@ -40,3 +41,15 @@ def room(request, pk):
     print('-----------')
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm();
+    if request.method == 'POST':
+        # print(request.POST)
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save();
+            return redirect('home')
+
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
