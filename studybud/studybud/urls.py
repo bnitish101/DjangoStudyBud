03844dc52d1c93e90463cc7b8152485680913041
Nitish <bnitish101@gmail.com>
2022-02-13 17:03:51 +0530

    template dir was not at path

--- studybud/studybud/urls.py
+++ studybud/studybud/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# from django.http import HttpResponse
+from django.urls import path, include
+
+# def home(request):
+#     return HttpResponse('Hi Main.')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('base.urls')),
 ]
