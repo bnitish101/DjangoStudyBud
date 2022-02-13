b675adb49ec387c010b9c7b85a2ec93d4f07ab5f
Nitish <bnitish101@gmail.com>
2022-02-13 18:18:06 +0530

    pass dynamic values in views and urls

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -3,8 +3,29 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'base/home.html')
+rooms = [
+    {'id':1, 'name':'Learn Python'},
+    {'id':2, 'name':'Learn Django'},
+    {'id':3, 'name':'Learn ReactJs'},
+    {'id':4, 'name':'Learn Flask'},
+]
 
-def room(request):
-    return render(request, 'base/room.html')
+def home(request):
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        # print('-----------')
+        # print(rooms)
+        # print('-----------')
+        # print(i)
+        # print('-----------')
+        # print(type(i))
+        # print('-----------')
+        if i['id'] == int(pk):
+            room = i['name']
+
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
