531ce04855018862c4687a5bb2b75e5b3cb9c06f
Nitish <bnitish101@gmail.com>
2022-02-14 19:19:21 +0530

    delete own comments only

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -193,3 +193,18 @@ def deleteRoom(request, pk):
         return redirect('home')
         
     return render(request, 'base/delete.html', {'obj': room})
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+    
+    # -------------- cb+ s -------------- #
+    if request.user != message.user:
+        return HttpResponse('You are not allowed to here!!!!!')
+    # -------------- cb+ e -------------- #
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+        
+    return render(request, 'base/delete.html', {'obj': message})
