ff131bcca574005eeb099ab8c6abbecee79aef7f
Nitish <bnitish101@gmail.com>
2022-03-03 23:02:03 +0530

    Django REST Framework configuration, created an api using django restframework

--- studybud/base/api/views.py
+++ studybud/base/api/views.py
@@ -1,10 +1,22 @@
-from django.http import JsonResponse
+# from django.http import JsonResponse
 
+# def getRoutes(request):
+#     routes = [
+#         'GET /api',
+#         'GET /api/room',
+#         'GET /api/room/:id',
+#     ]
+
+#     return JsonResponse(routes, safe=False)
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         'GET /api',
         'GET /api/room',
-        'GET /api/room/:id',
+        'GET /api/room/:id'
     ]
-
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
