a418f5d119d45ef471900d368c01e49c902a5ebe
Nitish <bnitish101@gmail.com>
2022-03-06 02:36:49 +0530

    updated user creatio form with fields = ['name', 'username', 'email', 'password1', 'password2']

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -5,9 +5,9 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 # from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message, User
-from .forms import RoomForm, UserForm
+from .forms import RoomForm, UserForm, MyUserCreationForm
 
 # Create your views here.
 
@@ -52,10 +52,12 @@ def logoutUser(request):
 
 def registerPage(request):
     # page = 'register'
-    form = UserCreationForm()
+    # form = UserCreationForm()
+    form = MyUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        # form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         print('------------')
         print(form.is_valid)
         print('------------')
